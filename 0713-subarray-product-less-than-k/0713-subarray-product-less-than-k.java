5611293327dc1952dcbaa18f188a524891ca2ab3
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-11-15 20:44:58 +0530

    Time: 4 ms (99.19%), Space: 46.9 MB (8.17%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,5 +1,9 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+        /*
+            0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+            ServiceNow - 8 | Facebook - 4 | SoFi - 2
+        */
         int l = 0;
         int cur_prod = 1;
         int res = 0;
