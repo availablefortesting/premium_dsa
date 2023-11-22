cc5faaa34a442ca3b1cad3c4770df68e369a2745
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-11-22 10:33:19 +0530

    Time: 36 ms (41.53%), Space: 69 MB (63.14%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,24 +1,52 @@
 class Solution {
-    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+    private int[] usingQueue(List<List<Integer>> nums) {
+        Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+        q.offer(new Pair(0,0));
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        while (!q.isEmpty()) {
+            var pos = q.poll();
+            int row = pos.getKey();
+            int col = pos.getValue();
+            res.add(nums.get(row).get(col));
+            
+            if (col == 0 && row + 1 < nums.size()) {
+                q.offer(new Pair(row+1, col));
+            }
+            
+            if (col + 1 < nums.get(row).size()) {
+                q.offer(new Pair(row, col + 1));
+            }
+        }
+        
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    private int[] usingHashMap(List<List<Integer>> nums) {
         HashMap<Integer,List<Integer>> m = new HashMap<>();
         ArrayList<Integer> res = new ArrayList<>();
-        int max_row_size = Integer.MIN_VALUE;
         
         for (int i = 0; i < nums.size(); i++) {
-            int row_size = nums.get(i).size();
-            max_row_size = Math.max(max_row_size, row_size);
-            for (int j = 0; j < row_size; j++) {
+            for (int j = 0; j < nums.get(i).size(); j++) {
                 int diag = i + j;
                 m.computeIfAbsent(diag, k -> new ArrayList<Integer>()).add(0, nums.get(i).get(j));
             }
         }
         
-        for (int i = 0; i < (nums.size() + max_row_size); i++) {
-            if (m.containsKey(i)) {
-                res.addAll(m.get(i));
+        int cur_diag = 0;
+        while (m.containsKey(cur_diag)) {
+            if (m.containsKey(cur_diag)) {
+                res.addAll(m.get(cur_diag));
             }
+            cur_diag++;
         }
         
         return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        // return usingHashMap(nums);
+        return usingQueue(nums);
     }
 }
