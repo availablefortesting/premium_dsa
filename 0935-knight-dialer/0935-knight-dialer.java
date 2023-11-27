2d439a52756f04764fecd94d67e3f8c5b1dc0f4d
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-11-27 17:31:02 +0530

    Time: 14 ms (93.21%), Space: 42.7 MB (91.03%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -1,22 +1,20 @@
 class Solution {
-    static HashMap<Integer, List<Integer>> m;
     int MOD = (int) 1e9 + 7;
-    
-    private static void prefillData() {
-        m = new HashMap<>();
-        
-        m.put(0, Arrays.asList(4,6)); m.put(1, Arrays.asList(6,8));
-        m.put(2, Arrays.asList(7,9)); m.put(3, Arrays.asList(4,8));
-        m.put(4, Arrays.asList(0,3,9)); m.put(5, Collections.EMPTY_LIST);
-        m.put(6, Arrays.asList(0,1,7)); m.put(7, Arrays.asList(2,6));
-        m.put(8, Arrays.asList(1,3)); m.put(9, Arrays.asList(2,4));
-    }
+    static int[][] jumps = {
+            {4, 6},
+            {6, 8},
+            {7, 9},
+            {4, 8},
+            {3, 9, 0},
+            {},
+            {1, 7, 0},
+            {2, 6},
+            {1, 3},
+            {2, 4}
+        };
     
     public int knightDialer(int n) {
-        if (n == 1) {
-            return 10;
-        }
-        prefillData();
+        if (n == 1) return 10;
         
         int[] prev = new int[10];
         Arrays.fill(prev, 1);
@@ -24,13 +22,13 @@ class Solution {
         while (n != 1) {
             int[] cur = new int[10];
             
-            for (int i = 0; i<10; i++) {
-                for (Integer next_pos : m.get(i)) {
+            for (int i = 0; i < 10; i++) {
+                for (int next_pos : jumps[i]) {
                     cur[next_pos] = (cur[next_pos] + prev[i]) % MOD; 
                 }
             }
             
-            prev = cur;
+            prev = cur; 
             n--;
         }
         
