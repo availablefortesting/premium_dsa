4b4b6f24486a1d8e12358dfbc365a6a9af58c55e
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-12-01 17:07:14 +0530

    Time: 40 ms (77.63%), Space: 16.4 MB (46.94%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -16,3 +16,15 @@ class Solution:
         node.right = self.bstFromPreorder(preorder[partition:])
         
         return node
+    
+    def bstFromPostorder(self, postorder: List[int]) -> Optional[TreeNode]:
+        if not postorder:
+            return None
+        
+        node = TreeNode(postorder.pop())
+        partition = bisect.bisect(postorder, node.val)
+        
+        node.right = self.bstFromPreorder(postorder[partition:])
+        node.left = self.bstFromPreorder(postorder[:partition])
+        
+        return node
