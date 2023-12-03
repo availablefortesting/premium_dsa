02c553425c4f9b322c78c9227f175b6e2afa13a2
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-12-04 00:05:14 +0530

    Edgyy insert-into-a-sorted-circular-linked-list

--- 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
+++ 0708-insert-into-a-sorted-circular-linked-list/0708-insert-into-a-sorted-circular-linked-list.java
@@ -30,10 +30,15 @@ class Solution {
         boolean toInsert = false;
         do {
           if (prev.val <= insertVal && insertVal <= curr.val) {
-            // Case 1).
+            // Case 1) The value of new node sits between the minimal and maximal values of the current list. 
+            //         As a result, it should be inserted within the list.
             toInsert = true;
           } else if (prev.val > curr.val) {
-            // Case 2).
+            /* Case 2). The value of new node goes beyond the minimal and maximal values of the current list, 
+                        either less than the minimal value or greater than the maximal value. 
+                        In either case, the new node should be added right after the tail node 
+                            (i.e. the node with the maximal value of the list).
+            */
             if (insertVal >= prev.val || insertVal <= curr.val)
               toInsert = true;
           }
@@ -48,7 +53,8 @@ class Solution {
         } while (prev != head);
         
         
-        // Case 3).
+        // Case 3). Finally, there is one case that does not fall into any of the above two cases. 
+        //            This is the case where the list contains uniform values.
         prev.next = new Node(insertVal, curr);
         return head;
     }
