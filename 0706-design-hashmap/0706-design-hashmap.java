7e88294146866980bf1b777a0a874e76b8d348e3
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2023-12-07 17:17:19 +0530

    Time: 53 ms (16.04%), Space: 56.8 MB (5.80%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,47 @@
+class MyHashMap {
+    private final int table_size = 10001;
+    private final int bucket_size = 101;
+    private int[][] map;
+    
+    public MyHashMap() {
+        this.map = new int[this.table_size][this.bucket_size];
+        System.out.println(this.map.length);
+        for (int[] row: this.map)
+            Arrays.fill(row, -1);
+    }
+    
+    // private int[] hash(int key) {
+    //     return new int[]{
+    //         key / this.bucket_size,
+    //         key % this.bucket_size
+    //     };
+    // }
+    
+    private int getBucketNo(int key) {
+        return key / this.bucket_size;
+    }
+    
+    private int getBucketKey(int key) {
+        return key % this.bucket_size;
+    }
+    
+    public void put(int key, int value) {
+        this.map[getBucketNo(key)][getBucketKey(key)] = value;
+    }
+    
+    public int get(int key) {
+        return this.map[getBucketNo(key)][getBucketKey(key)];
+    }
+    
+    public void remove(int key) {
+        this.map[getBucketNo(key)][getBucketKey(key)] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
