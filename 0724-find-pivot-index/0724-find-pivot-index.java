4431bb8a8191ac10b8c892b8a60311330485c86f
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-01-03 00:03:57 +0530

    Time: 2 ms (21.77%), Space: 45.5 MB (13.03%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -5,12 +5,9 @@ class Solution {
             sum += n;
             
         int left_sum = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; left_sum += nums[i++])
             if (2 * left_sum == sum - nums[i])
                 return i;
-            
-            left_sum += nums[i];
-        }
         
         return -1;
     }
