8292268c9438bb30de453225f10610ba4c126a4a
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-01-03 15:09:55 +0530

    Time: 1 ms (100.00%), Space: 41.7 MB (11.00%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -18,7 +18,7 @@ class Solution {
     }
     
     public int lastRemaining(int n) {
-        return simulation(n);
-        // return n == 1 ? 1 : 2 * (n/2 + 1 - lastRemaining(n/2));
+        // return simulation(n);
+        return n == 1 ? 1 : 2 * (n/2 + 1 - lastRemaining(n/2));
     }
 }
