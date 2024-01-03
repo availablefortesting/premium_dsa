ea22380d851a46f33c8a28662e0782b9f6e44cb7
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-01-03 14:16:51 +0530

    Time: 1 ms (67.22%), Space: 40.2 MB (50.00%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,5 +1,16 @@
 class Solution {
     // Josepuse problem
+    private int iterative(int n, int k) {
+        int cur_n = 1, cur_winner = 0;
+        
+        while (cur_n <= n) {
+            cur_winner = (cur_winner + k) % cur_n;
+            cur_n += 1;
+        }
+        
+        return cur_winner + 1;
+    }
+    
     private int solve(int n, int k) {
         if (n == 1) 
             return 0;
@@ -8,6 +19,7 @@ class Solution {
     }
     
     public int findTheWinner(int n, int k) {
-        return solve(n,k) + 1;
+        // return solve(n,k) + 1;
+        return iterative(n,k);
     }
 }
