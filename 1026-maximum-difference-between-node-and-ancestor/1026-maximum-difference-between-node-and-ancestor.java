2c432ca9d3a9d3e17530b2558f7f11054e326b95
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-01-11 13:29:27 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (7.37%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -2,24 +2,18 @@ class Solution {
     int maxDiff;
     public int maxAncestorDiff(TreeNode root) {
         maxDiff = 0;
-        dfs(root);
+        dfs(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
         return maxDiff;
     }
     
-    private int[] dfs(TreeNode root) {
-        if (root.left == null && root.right == null)   return new int[]{root.val, root.val};
-        
-        int[] left = root.left != null ? dfs(root.left) : new int[]{root.val, root.val};
-        int[] right = root.right != null ? dfs(root.right) : new int[]{root.val, root.val};
-        int min_left = left[0], max_left = left[1];
-        int min_right = right[0], max_right = right[1];
-        
-        maxDiff = Math.max(maxDiff, Math.max(Math.abs(root.val - min_left), Math.abs(root.val - min_right)));
-        maxDiff = Math.max(maxDiff, Math.max(Math.abs(root.val - max_left), Math.abs(root.val - max_right)));
-        
-        // System.out.println(root.val + " : " + min_left + " , " + min_right + " <> " + maxDiff);
-        int cur_min = Math.min(root.val, Math.min(min_left, min_right));
-        int cur_max = Math.max(root.val, Math.max(max_left, max_right));
-        return new int[]{cur_min, cur_max};
+    private void dfs(TreeNode root, int min, int max) {
+        if (root != null) {
+            min = Math.min(min, root.val);
+            max = Math.max(max, root.val);
+            maxDiff = Math.max(maxDiff, max - min);
+            
+            dfs(root.left, min, max);
+            dfs(root.right, min, max);
+        }
     }
 }
