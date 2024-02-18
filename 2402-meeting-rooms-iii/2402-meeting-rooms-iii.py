99fdd7757172bd637b278ecaac2256ab0608fb01
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-02-18 18:20:56 +0530

    Time: 1232 ms (47.02%), Space: 62.5 MB (78.07%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,41 +1,36 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort()
+        
         rooms_q = [room for room in range(n)]
         heapq.heapify(rooms_q)
         
         cur_meetings = []
-        freq = defaultdict(int)
+        freq = [0] * n
         
         for st,et in meetings:
-            if not cur_meetings:
-                heapq.heappush(cur_meetings, [et, heapq.heappop(rooms_q)])
-                freq[0] += 1
-            else:
-                while cur_meetings and cur_meetings[0][0] <= st:
-                    heapq.heappush(rooms_q, heapq.heappop(cur_meetings)[1])
+            # release finished meetings rooms
+            while cur_meetings and cur_meetings[0][0] <= st:
+                heapq.heappush(rooms_q, heapq.heappop(cur_meetings)[1])
+
+            room_id = None
+            # schedule a meeting in new room if there is
+            if rooms_q:
+                room_id = heapq.heappop(rooms_q)
+                heapq.heappush(cur_meetings, [et, room_id])
                 
-                if not cur_meetings:
-                    free_room = heapq.heappop(rooms_q)
-                    heapq.heappush(cur_meetings, [et, free_room])
-                    freq[free_room] += 1
-                else:
-                    if not rooms_q:
-                        end_t, room_id = heapq.heappop(cur_meetings)
-                        heapq.heappush(cur_meetings, [end_t + (et - st), room_id])
-                        freq[room_id] += 1
-                    else:
-                        free_room = heapq.heappop(rooms_q)
-                        heapq.heappush(cur_meetings, [et, free_room])
-                        freq[free_room] += 1
-            # print(cur_meetings, rooms_q)
-        
-        # print(freq)
+            # else schedule in a room which is earliest free
+            else:
+                earliest_et, room_id = heapq.heappop(cur_meetings)
+                heapq.heappush(cur_meetings, [earliest_et + (et - st), room_id])
+            
+            freq[room_id] += 1
+
+        # find max-used room among all rooms
         max_used, max_used_room = 0, 0
-        for room, f in freq.items():
+        for room, f in enumerate(freq):
             if max_used < f:
-                max_used = f
-                max_used_room = room
+                max_used, max_used_room = f, room
                 
         return max_used_room
                 
