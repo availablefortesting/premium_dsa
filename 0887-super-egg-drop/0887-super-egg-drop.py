ca4bf5d5d72097bf139305c4b8ec65f1b2a8b843
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2024-04-15 10:53:19 +0530

    Time: 184 ms (54.07%), Space: 24.9 MB (37.21%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.py
+++ 0887-super-egg-drop/0887-super-egg-drop.py
@@ -3,10 +3,10 @@ class Solution:
         # approach this problem as with M moves, for how much floor
         # we can give certainty, with given eggs
         
-        m = 300 # assume max
-        dp = [[0 for _ in range(k + 1)] for _ in range(m + 1)]
+        # for N floor max moves can be N
+        dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
         
-        for moves in range(1, m + 1):
+        for moves in range(1, n + 1):
             for eggs in range(1, k + 1):
                 dp[moves][eggs] = 1 + (dp[moves-1][eggs-1] + dp[moves-1][eggs])
                 if dp[moves][eggs] >= n:
