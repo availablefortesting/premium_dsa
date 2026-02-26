66561d89caf01ff3bcd43f78dca1711d51eb1dd8
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-02-26 22:40:21 +0530

    Time: 0 ms (100%), Space: 42.7 MB (88.5%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,28 +1,15 @@
 class Solution {
     public int numSteps(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int steps = 0;
+        int carry = 0;
+        int res = 0;
         
-        while(sb.length() != 1) {
-            // even
-            if (sb.charAt(sb.length() - 1) == '0') {
-                int idx = sb.lastIndexOf("1");
-                
-                steps += (sb.length() - idx - 1);
-                sb.delete(idx + 1, sb.length());
-            } else {
-                steps += 1;
-                    
-                int i = sb.length() - 1;
-                while (i >= 0 && sb.charAt(i) == '1')
-                    sb.setCharAt(i--, '0');
-                
-                if (i != -1) sb.setCharAt(i, '1');
-                else    sb.insert(0, '1');
+        for (int i = s.length() - 1; i >= 1; i--) {
+            if (s.charAt(i) - '0' + carry == 1) {
+                carry = 1;
+                res++;
             }
-            System.out.println(sb.toString());
+            res++;
         }
-        
-        return steps;
+        return res + carry;
     }
 }
