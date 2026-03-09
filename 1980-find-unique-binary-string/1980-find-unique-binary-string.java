d7ae643393162799b65b5e8c1783452c4fe85940
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-03-09 10:34:23 +0530

    Time: 0 ms (100%), Space: 43.1 MB (76.75%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,16 +1,21 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        int n = nums.length;
-        String res = "0".repeat(n);
-        Set<Integer> nums_set = new HashSet<>();
-        for (String num : nums) nums_set.add(Integer.parseInt(num, 2));
+        StringBuilder ans= new StringBuilder();                  
+        for(int i=0; i<nums.length; i++)  
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        return ans.toString();
+
+        // int n = nums.length;
+        // String res = "0".repeat(n);
+        // Set<Integer> nums_set = new HashSet<>();
+        // for (String num : nums) nums_set.add(Integer.parseInt(num, 2));
         
-        if (!nums_set.contains(0))    return res;
-        for (int i = (int)Math.pow(2, n - 1); i < (int)Math.pow(2, n); i++) {
-            if (!nums_set.contains(i)) {
-                return Integer.toBinaryString(i);
-            }
-        }
-        return res;       
+        // if (!nums_set.contains(0))    return res;
+        // for (int i = (int)Math.pow(2, n - 1); i < (int)Math.pow(2, n); i++) {
+        //     if (!nums_set.contains(i)) {
+        //         return Integer.toBinaryString(i);
+        //     }
+        // }
+        // return res;       
     }
 }
