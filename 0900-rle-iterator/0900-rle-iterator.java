48fe956b60889ed5826fc85caebcaadf698575e8
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-03-15 20:05:19 +0530

    Time: 4 ms (99.54%), Space: 44.2 MB (42.69%) - LeetHub

--- 0900-rle-iterator/0900-rle-iterator.java
+++ 0900-rle-iterator/0900-rle-iterator.java
@@ -1,33 +1,23 @@
 class RLEIterator {
-    Deque<Integer> queue;
-    int n;
+    int[] data;
+    int index;
 
     public RLEIterator(int[] encoding) {
-        this.n = encoding.length;
-        this.queue = new ArrayDeque<Integer>();
-
-        for(int i = 0; i < this.n; i++)
-            this.queue.addLast(encoding[i]);  
+        data = encoding; index = 0; 
     }
     
     public int next(int n) {
-        int freq = 0, num = 0, temp = 0;
-        while(n != 0 && this.queue.size() > 0) {
-            freq = this.queue.pollFirst();
-            num = this.queue.pollFirst();
-
-            temp = n;
-            n = Math.max(temp - freq, 0);
-            freq = Math.max(freq - temp, 0);
-
-            if (freq != 0) {
-                this.queue.addFirst(num);
-                this.queue.addFirst(freq);
+        while (index < data.length && n > 0) {
+            int freq = data[index];
+            if (freq >= n) {
+                data[index] -= n;
+                return data[index + 1];
+            } else {
+                n -= freq;
+                data[index] = 0;
+                index += 2;
             }
-
-            if (n == 0) return num;
         }
-
         return -1;
     }
 }
