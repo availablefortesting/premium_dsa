746c2bd4029951dea2e15d70d7586674a7ee3729
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-04-07 02:51:47 +0530

    Time: 3 ms (9.09%), Space: 42.6 MB (76.32%) - LeetHub

--- 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
@@ -1,18 +1,37 @@
 class Solution {
     public String strWithout3a3b(int a, int b) {
-        return helper(a, b, "a", "b");
-    }
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] - x[0]);
 
-    public static String helper(int a, int b, String aa, String bb) {
-        if (a < b)
-            return helper(b, a, bb, aa);
-        
-        if (b == 0)
-            return aa.repeat(Math.min(2, a));
-        
-        int use_a = Math.min(2, a);
-        use_a = a - use_a >= b ? use_a : 1;
+        if (a > 0) pq.add(new int[]{a, 'a'});
+        if (b > 0) pq.add(new int[]{b, 'b'});
 
-        return aa.repeat(use_a) + bb + helper(a - use_a, b - 1, aa, bb);
+        while (!pq.isEmpty()) {
+
+            int[] first = pq.poll();
+            int len = sb.length();
+
+            // If last two chars same as current → cannot use
+            if (len >= 2 &&
+                sb.charAt(len - 1) == first[1] &&
+                sb.charAt(len - 2) == first[1]) {
+
+                if (pq.isEmpty()) break;
+
+                int[] second = pq.poll();
+                sb.append((char) second[1]);
+                second[0]--;
+
+                if (second[0] > 0) pq.add(second);
+                pq.add(first); // put first back
+            } else {
+                sb.append((char) first[1]);
+                first[0]--;
+                if (first[0] > 0) pq.add(first);
+            }
+        }
+
+        return sb.toString();
     }
 }
+
