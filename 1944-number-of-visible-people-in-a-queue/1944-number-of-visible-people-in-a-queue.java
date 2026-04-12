79f686330c50d01d5ae40195b375fac10ad87b72
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-04-13 00:29:36 +0530

    Time: 21 ms (90.91%), Space: 116.4 MB (40.91%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -1,20 +1,47 @@
 class Solution {
     public int[] canSeePersonsCount(int[] heights) {
+        return canSeePersonsCount2(heights);
+
+        // int n = heights.length;
+        // Stack<Integer> monoDecreasing = new Stack<>();
+        // int[] res = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     while (!monoDecreasing.isEmpty() && heights[monoDecreasing.peek()] <= heights[i])
+        //         res[monoDecreasing.pop()]++;    // guy can see till only curr_i (which is tallest)
+
+        //     // Now as curr_i will be smaller than stack.peek(), so peek can see curr_i
+        //     if (!monoDecreasing.isEmpty())
+        //         res[monoDecreasing.peek()]++;
+
+        //     monoDecreasing.push(i);
+        // }
+
+        // return res;
+    }
+
+    public int[] canSeePersonsCount2(int[] heights) {
         int n = heights.length;
-        Stack<Integer> monoDecreasing = new Stack<>();
-        int[] res = new int[n];
+        int ans[] = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            while (!monoDecreasing.isEmpty() && heights[monoDecreasing.peek()] <= heights[i])
-                res[monoDecreasing.pop()]++;    // guy can see till only curr_i (which is tallest)
+        Deque<Integer> stack = new ArrayDeque<>();
 
-            // Now as curr_i will be smaller than stack.peek(), so peek can see curr_i
-            if (!monoDecreasing.isEmpty())
-                res[monoDecreasing.peek()]++;
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 0;
 
-            monoDecreasing.push(i);
+            // count and pop all the smaller person, compare to current
+            while (!stack.isEmpty() && heights[i] > stack.peek()) {
+                count++;
+                stack.pop();
+            }
+
+            // If someone taller exists → also visible
+            if (!stack.isEmpty())   count++;
+
+            ans[i] = count;
+            stack.push(heights[i]);
         }
 
-        return res;
+        return ans;
     }
 }
