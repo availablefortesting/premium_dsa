4d4b9db15de4b9a31305cbbc925c158efeb81586
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-04-20 02:19:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,23 +1,27 @@
 class Solution {
+    char[] chars;
+
     public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // empty string
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
-        char prev = s.charAt(0), cur = '#';
-        
-        for (int i = 2; i < n + 1; i++) {
-            cur = s.charAt(i - 1);
-            if (cur != '0') {
-                dp[i] += dp[i - 1];
-            }
-            if (prev == '1' || prev == '2' && cur <= '6') {
-                dp[i] += dp[i - 2];
-            }
-            
-            prev = cur;
+        this.chars = s.toCharArray();
+        return dfs(0, s.length() - 1);
+    }
+
+    private int dfs(int i, int j) {
+        if (i > j) return 1; // decoded everything
+        if (chars[i] == '0') return 0; // can't start with 0
+        if (i == j) return 1; // single non-zero char
+
+        int res = 0;
+
+        // take 1 digit
+        res += dfs(i + 1, j);
+
+        // take 2 digits if valid (10..26)
+        int val = (chars[i] - '0') * 10 + (chars[i + 1] - '0');
+        if (val >= 10 && val <= 26) {
+            res += dfs(i + 2, j);
         }
-        
-        return dp[n];
+
+        return res;
     }
 }
