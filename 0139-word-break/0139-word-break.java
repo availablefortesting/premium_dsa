58e15541949eafa2b4c03a44f27535893677b34d
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-05-14 01:43:15 +0530

    Time: 3 ms (81.2%), Space: 43.1 MB (87.66%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,48 +1,23 @@
 class Solution {
-    private boolean recursive(String s, List<String> wordDict, int i) {
-        if (i < 0) 
-            return true;
-        
-        // if (memo[i] != -1) {
-        //     return memo[i] == 1;
-        // }
-        
-        for (String word: wordDict) {
-            // Handle out of bounds case
-            if (i - word.length() + 1 < 0) {
-                continue;
-            }
-            
-            if (s.substring(i - word.length() + 1, i + 1).equals(word) && recursive(s, wordDict, i - word.length())) {
-                // memo[i] = 1;
-                return true;
-            }
-        }
-        
-        // memo[i] = 0;
-        return false;
-    }
-    
     // O(n * m * k)
-    private boolean dp(String s, List<String> wordDict) {
+    public boolean wordBreak(String s, List<String> wordDict) {
         int n = s.length();
-        boolean[] dp = new boolean[n+1];
-        dp[n] = true;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
         
-        for (int i = n-1; i >= 0; --i) {
+        for (int i = 1; i <= n; ++i) {
             for (String word : wordDict) {
-                if ( (i + word.length()) <= n && s.substring(i, i + word.length()).equals(word) ) {
-                    dp[i] = dp[i+word.length()];
+                int wordLen = word.length();
+                // Check if we can form substring up to position i using this word
+                if (i >= wordLen && dp[i - wordLen]) {
+                    if (s.substring(i - wordLen, i).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
                 }
-                if (dp[i] == true)  break;
             }
         }
         
-        return dp[0];
-    }
-    
-    public boolean wordBreak(String s, List<String> wordDict) {
-        return dp(s, wordDict);
-        // return recursive(s, wordDict, s.length()-1);
+        return dp[n];
     }
 }
