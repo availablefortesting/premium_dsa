349d40950ab9dfdf525671cd9390533ee8cdddb5
availablefortesting <126980701+availablefortesting@users.noreply.github.com>
2026-05-14 00:35:07 +0530

    Time: 0 ms (100%), Space: 44.8 MB (24.96%) - LeetHub

---
+++ 0256-paint-house/0256-paint-house.java
@@ -0,0 +1,31 @@
+class Solution {
+    private int[][] costs;
+    private int n, m;
+    private Integer[][] memo;
+
+    public int minCost(int[][] costs) {
+        this.costs = costs;
+        this.n = costs.length;
+        this.m = costs[0].length;
+        
+        this.memo = new Integer[n][m + 1]; // +1 to handle prevCol = -1
+        
+        return helper(0, -1);
+    }
+
+    private int helper(int i, int prevCol) {
+        if (i == n) return 0;
+        
+        int memoIdx = prevCol + 1;
+        if (memo[i][memoIdx] != null)   return memo[i][memoIdx];
+        
+        int res = Integer.MAX_VALUE;
+        for (int c = 0; c < m; c++) {
+            if (c == prevCol) continue;
+            
+            res = Math.min(res, costs[i][c] + helper(i + 1, c));
+        }
+        
+        return memo[i][memoIdx] = res;
+    }
+}
